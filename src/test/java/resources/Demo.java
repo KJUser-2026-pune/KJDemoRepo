3854adb549fb3c6c3c858d1623926c43c47e0516
KJUser-2026-pune <komalranaware128@gmail.com>
2026-01-19 12:25:44 +0530

    commited in master

---
+++ src/test/java/resources/Demo.java
@@ -0,0 +1,16 @@
+package resources;
+
+import org.junit.Test;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+
+}
